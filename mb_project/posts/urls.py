a1d215307cab476d88d5c7d3f244b8e550f74e67
ARNAB DAS <raj713335@gmail.com>
2020-12-21 23:32:40 +0530

    Initial Commit

---
+++ mb_project/posts/urls.py
@@ -0,0 +1,10 @@
+# mb_project/urls.py
+
+from django.contrib import admin
+from django.urls import path,include
+from .views import HomePageView
+
+urlpatterns=[
+    path('',HomePageView.as_view(),name='home'),
+
+]
