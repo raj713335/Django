a397b14dea0dd13dd67c15054d4ef006ef78f37c
ARNAB DAS <raj713335@gmail.com>
2020-12-21 20:45:32 +0530

    Initial Commit

--- pages_project/pages/tests.py
+++ pages_project/pages/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+#pages./tests.py
+
+from django.test import TestCase,SimpleTestCase
 
 # Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response=self.client.get('/')
+        self.assertEqual(response.status_code,200)
+
+    def test_about_page_status_code(self):
+        response=self.client.get('/about/')
+        self.assertEqual(response.status_code,200)
