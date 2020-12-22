a9014e961fe09b64a2ab5030bde4f3ea1c86825e
ARNAB DAS <raj713335@gmail.com>
2020-12-22 17:00:58 +0530

    Initial Commit

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+
+
+LOGIN_REDIRECT_URL='home'
+
+LOGOUT_REDIRECT_URL='home'
