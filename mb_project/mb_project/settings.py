9b50a71d2fab8e3c89b87a22ea58cc8b6cc81899
ARNAB DAS <raj713335@gmail.com>
2020-12-22 11:21:35 +0530

    Initial Commit

--- mb_project/mb_project/settings.py
+++ mb_project/mb_project/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'mb_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templatescd ..'
+                                       '')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
