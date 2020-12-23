1efefdb4235c814e172bab102fae823c07a2b58f
ARNAB DAS <raj713335@gmail.com>
2020-12-24 00:31:02 +0530

    Initial Commit

--- newspaper_project/articles/urls.py
+++ newspaper_project/articles/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('<int:pk>/edit/',ArticleUpdateView.as_view(), name='article_edit'), # new
     path('<int:pk>/',ArticleDetailView.as_view(), name='article_detail'), # new
     path('<int:pk>/delete/',ArticleDeleteView.as_view(), name='article_delete'), # new
-    path('new/',ArticleCreateView.as_view(),name='article_name'),
+    path('new/',ArticleCreateView.as_view(),name='article_new'),
     path('', ArticleListView.as_view(), name='article_list'),
 ]
--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,5 +1,6 @@
 # articles/views.py
 
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views.generic import ListView,DetailView
 from django.views.generic.edit import UpdateView,DeleteView,CreateView
 from django.urls import reverse_lazy
@@ -10,28 +11,48 @@ from django.shortcuts import render
 
 # Create your views here.
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     model = Article
     template_name = 'article_list.html'
+    login_url = 'login'
 
 
-class ArticleDetailView(DetailView): # new
+class ArticleDetailView(LoginRequiredMixin,DetailView): # new
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
-class ArticleUpdateView(UpdateView): # new
+
+class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView): # new
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
+    login_url = 'login'
+
+    def test_func(self):
+        obj=self.get_object()
+        return obj.author==self.request.user
 
 
-class ArticleDeleteView(DeleteView): # new
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView): # new
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
+
+    def test_func(self):
+        obj=self.get_object()
+        return obj.author==self.request.user
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
