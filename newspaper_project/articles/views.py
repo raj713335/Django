73564ee53b899e4357d22f8040aa9bc65320d7d5
ARNAB DAS <raj713335@gmail.com>
2020-12-23 13:33:40 +0530

    Initial Commit

--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,3 +1,12 @@
+# articles/views.py
+
+from django.views.generic import ListView
+from .models import Article
+
 from django.shortcuts import render
 
 # Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
--- newspaper_project/newspaper_project/urls.py
+++ newspaper_project/newspaper_project/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')), # new
     path('users/', include('django.contrib.auth.urls')), # new
+    path('articles/',include('articles.urls')),
     #path('', TemplateView.as_view(template_name='home.html'),name='home'),
     path('',include('pages.urls')),
 ]
