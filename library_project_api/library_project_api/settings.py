abd296712009adbc2222554680753ffe1ec0f37f
ARNAB DAS <raj713335@gmail.com>
2020-12-24 15:14:24 +0530

    Initial Commit

--- library_project_api/library_project_api/settings.py
+++ library_project_api/library_project_api/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd Party
+    'rest_framework',
+
     'books.apps.BooksConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
